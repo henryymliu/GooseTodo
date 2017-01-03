79217eb3c6315e523dd534f62cc325349d894eed
Cheng <cheng.k.xie@gmail.com>
2017-01-03 13:00:00 -0800

    Starting to work on backend data storage.

---
+++ app/src/main/java/com/dontknowwhattocallthis/motivationaltasklist/model/Task.java
@@ -0,0 +1,9 @@
+package com.dontknowwhattocallthis.motivationaltasklist.model;
+
+/**
+ * Created by Cheng on 27/12/2016.
+ */
+
+public class Task {
+    public getName
+}
---
+++ app/src/main/java/com/dontknowwhattocallthis/motivationaltasklist/persistence/PersistingResource.java
@@ -0,0 +1,9 @@
+package com.dontknowwhattocallthis.motivationaltasklist.persistence;
+
+/**
+ * Created by Cheng on 27/12/2016.
+ */
+
+public interface PersistingResource {
+
+}
